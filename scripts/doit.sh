572228d62f6be36d40065869a474b8f39cae334d
maybetree <maybetree48@proton.me>
2026-07-22 11:57:09 +0200

    doit.sh skip built packages

--- scripts/doit.sh
+++ scripts/doit.sh
@@ -4,8 +4,25 @@ eecho() { echo "$@" >&2 ; }
 pecho() { printf '%s' "$*" ; }
 die() { eecho "$@"; exit 1; }
 edie() { die "Failed to: $*"; }
-cd_ass() { cd "$1" || die "Failed to cd into ${2:-$1}" ; }
-ass() { "$@" || die "Failed to run command: $*" ; }
+try() {
+	"$@" || {
+		eecho ''
+		eecho "Failed to run: '$*'"
+		eecho ''
+		kill "$$"
+	}
+}
+mtry() {
+	msg="$1"
+	shift
+	"$@" || {
+		eecho ''
+		eecho "Failed: '$msg'"
+		eecho "(command: $* )"
+		eecho ''
+		kill "$$"
+	}
+}
 
 upstream_path=./void-packages-upstream
 upstream_url=https://github.com/void-linux/void-packages
@@ -13,17 +30,18 @@ merged_path=./merged
 self_packages_path=./void-packages
 upper_path=./upper
 work_path=./work
+binpkgs_path=$merged_path/hostdir/binpkgs/
 
 get_upstream() {
 	if [ ! -r "$upstream_path" ]
 	then
 		eecho "Cloning $upstream_url into $upstream_path ..."
-		git clone "$upstream_url" "$upstream_path" \
-			|| edie "clone void-packages repo"
+		mtry "clone void-packages repo" \
+			git clone "$upstream_url" "$upstream_path"
 	else
 		eecho "void-packages already cloned, pulling..."
-		git -C "$upstream_path" pull \
-			|| edie "pull void-packages repo"
+		mtry "pull void-packages repo" \
+			git -C "$upstream_path" pull
 	fi
 }
 
@@ -34,36 +52,44 @@ ensure_unshare() {
 		eecho "should be able to mount overlayfs."
 	else
 		eecho "Am not root, unsharing..."
-		unshare -rUm -- "$@" \
-			|| edie "unshare"
+		mtry "unshare" unshare -rUm -- "$@"
 	fi
 }
 
 mount_overlay() {
 	eecho "mounting overlay..."
-	mkdir -p "$merged_path" \
-		|| edie "make mountpoint"
-	mkdir -p "$upper_path" \
-		|| edie "make upper dir"
-	mkdir -p "$work_path" \
-		|| edie "make work dir"
-	mount -t overlay overlay \
+	mtry "make mountpoint" mkdir -p "$merged_path"
+	mtry "make upper dir" mkdir -p "$upper_path"
+	mtry "make work dir" mkdir -p "$work_path"
+	mtry "mount overlay" \
+		mount -t overlay overlay \
 		"-olowerdir=$self_packages_path:$upstream_path,upperdir=$upper_path,workdir=$work_path" \
-		"$merged_path" \
-		|| edie "mount overlay"
+		"$merged_path"
 }
 
 build_packages() {
 	eecho "building packages..."
-	cd_ass "$merged_path"
+	#cd_ass "$merged_path"
 	export XBPS_ALLOW_CHROOT_BREAKOUT=1
-	./xbps-src binary-bootstrap \
-		|| edie "xbps binary-bootstrap"
+	(
+		try cd "$merged_path"
+		mtry "xbps binary-bootstrap" ./xbps-src binary-bootstrap
+	)
 
-	for ver in 3.14 3.13 3.12 3.11 3.10
+	#for ver in 3.14 3.13 3.12 3.11 3.10
+	for pkg in $(try cd ./void-packages/srcpkgs ; try ls ; )
 	do
-		./xbps-src pkg freshsnakes-python$ver \
-			|| edie "make package"
+		#pkg="$(pecho "$pkg" | try cut -d/ -f4)"
+		eecho "building: $pkg"
+		if [ -e $binpkgs_path/$pkg*.xbps ]
+		then
+			eecho "$pkg: already built"
+			continue
+		fi
+		(
+			try cd "$merged_path"
+			mtry "make pkg" ./xbps-src pkg "$pkg"
+		)
 	done
 }
 
