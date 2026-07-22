f231387b726b9da7a8a6a0be91c7caae6d23ed57
maybetree <maybetree48@proton.me>
2026-07-22 11:40:09 +0200

    add changed-pkgs script

---
+++ scripts/changed-pkgs.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+eecho() { echo "$@" >&2 ; }
+pecho() { printf '%s' "$*" ; }
+die() { eecho "$@"; exit 1; }
+edie() { die "Failed to: $@"; }
+try() {
+	eval "$@" || {
+		eecho "Failed to run: '$*'"
+		kill "$$"
+	}
+}
+mtry() {
+	msg="$1"
+	shift
+	eval "$@" || {
+		eecho "Failed: '$msg'"
+		eecho "(command: $* )"
+		kill "$$"
+	}
+}
+
+pkgsdir="./void-packages/srcpkgs"
+
+strip_dotslash() {
+	sed 's|^./||' || edie "strip dotslash"
+}
+
+changed_pkgs() {
+	pkgs="$(
+		try git diff \
+			--diff-filter=d \
+			--name-only \
+			HEAD~1..HEAD \
+			-- "$pkgsdir" \
+		| try strip_dotslash \
+		| cut -d '/' -f 3 \
+		| try sort \
+		| try uniq \
+	)"
+
+	#dirs="$(
+	#	try find "$pkgsdir" -maxdepth 1 -mindepth 1 -type d \
+	#	| try strip_dotslash \
+	#	| try sort \
+	#	| try uniq
+	#)"
+
+	pecho "$pkgs"
+	#eecho "$dirs"
+
+
+}
+
+changed_pkgs
